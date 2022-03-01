b5d25f86a048395d57f8587f17bcf7a47dcc26d6
w41ter <w41ter.l@gmail.com>
2022-03-01 21:28:17 +0800

    third: add gflags

--- cmake/FetchThirdParty.cmake
+++ cmake/FetchThirdParty.cmake
@@ -2,5 +2,6 @@ include(FetchContent)
 
 include(cmake/abseil.cmake)
 include(cmake/googletest.cmake)
+include(cmake/gflags.cmake)
 include(cmake/protobuf.cmake)
 include(cmake/glog.cmake)
---
+++ cmake/gflags.cmake
@@ -0,0 +1,6 @@
+FetchContent_Declare(
+    gflags
+    GIT_REPOSITORY https://github.com/gflags/gflags.git
+    GIT_TAG "v2.2.2"
+)
+FetchContent_MakeAvailable(gflags)
